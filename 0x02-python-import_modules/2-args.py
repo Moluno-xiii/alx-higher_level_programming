9141f6e4469216b510218264a4b5584a8653b3a9
moluno_xiii <molunoprogress@gmail.com>
2023-11-30 13:26:56 -0800

    task 2 redo

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """print the index and arguments"""
     import sys
-
-    arg_count = len(sys.argv) - 1
-    if arg_count == 0:
+    a = len(sys.argv)
+    if a == 1:
         print("0 arguments.")
-    elif arg_count == 1:
-        print("1 argument:")
     else:
-        print("{} arguments:".format(arg_count))
-
-    for i in range(arg_count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        if a == 2:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(a - 1))
+        for i in range(1, a):
+            print("{}: {}".format(i, sys.argv[i]))
