cb788b5caa5e268dffe18c33f979cb90e9eaff73
moluno_xiii <molunoprogress@gmail.com>
2024-03-05 03:56:19 -0800

    task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+--script that lists all records of the second tble in a database
+SELECT score, name FROM second_table ORDER BY score DESC;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,10 @@
 -- script that creates a second table and add multiple rows
-CREATE IF NOT EXISTS  second_table (id int, name VARCHAR(256), score int);
-INSERT INTO second_table (id, name, score) VALUES
-(1, "John", 10),
-(1, 'Alex', 3),
-(3, 'Bob', 14),
-(4, 'George', 8)
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+	);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(4, "George", 8);
