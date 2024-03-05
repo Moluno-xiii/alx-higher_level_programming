031876129307505f5b2d1010a92ea41ad0e9c9b2
moluno_xiii <molunoprogress@gmail.com>
2024-03-05 03:36:39 -0800

    task 7

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
 --  script that prints the full decription of the table in mySQL server
-USE hbtn_0c_0
 SHOW CREATE TABLE first_table
--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,2 @@
 -- script that lists all columns from a table in mySQL server
-USE hbtn_0c_0
-SELECT FROM first_table
+SELECT * FROM first_table
