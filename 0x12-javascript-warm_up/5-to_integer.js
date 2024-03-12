f31c06188ecf9938a0f244fce18dfc8f9d556176
moluno_xiii <molunoprogress@gmail.com>
2024-03-11 18:44:46 -0700

    task 5 and 6 corre

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const argc = process.argv[2];
 if (!isNaN(argc)){
-	console.log('My number:', argc)
-}
-else console.log('Not a number')
+	console.log('My number:', argc);
+};
+else console.log('Not a number');
