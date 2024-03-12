d180f6800c2800b7fc334a49da567c565ce8edee
moluno_xiii <molunoprogress@gmail.com>
2024-03-11 18:58:13 -0700

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const firstNumber = Number(process.argv[2]);
 const secondNumber = Number(process.argv[3]);
-
-function addNumbers (firstNumber, secondNumber){
-	return(firstNumber + secondNumber)
+function addNumbers (firstNumber, secondNumber) {
+	return(firstNumber + secondNumber);
 }
-
-console.log(addNumbers(firstNumber, secondNumber))
+console.log(addNumbers(firstNumber, secondNumber));
