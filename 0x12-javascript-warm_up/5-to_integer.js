3f1cb02f7e7fbc8ba55a4cc0f3e05b01933a4281
moluno_xiii <molunoprogress@gmail.com>
2024-03-11 19:00:41 -0700

    correction

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
+
 const argc = process.argv[2];
+
 if (!isNaN(argc)) {
 	console.log('My number:', argc);
 } else {
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
+
 const x = process.argv[2];
+
 if (isNaN(x)) {
         console.log('Missing size');
 } else {
