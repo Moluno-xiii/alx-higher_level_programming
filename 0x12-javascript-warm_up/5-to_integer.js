7979b18178f8897c901172b29c3beff5652ef33c
moluno_xiii <molunoprogress@gmail.com>
2024-03-11 18:47:31 -0700

    tasks 5, 6 and 7 corrected.

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const argc = process.argv[2];
-if (!isNaN(argc)){
+if (!isNaN(argc)) {
 	console.log('My number:', argc);
-};
+}
 else console.log('Not a number');
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const x = process.argv[2]
-if (isNaN(x)){
-	console.log("Missing number of occurences");
+const x = process.argv[2];
+if (isNaN(x)) {
+	console.log('Missing number of occurences');
 }
 else {
-	for (i = 0; i < x; i++){
-		console.log("C is fun")
+	for (i = 0; i < x; i++) {
+		console.log('C is fun');
 	}
 }
