91ff91e8d464670f9b799bae37e849fc710080f7
moluno_xiii <molunoprogress@gmail.com>
2024-03-12 02:50:20 -0700

    tasl 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/js
+module.exports = class Rectangle {
+	  constructor (w, h) {
+    if (w > 1 && h > 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = this.height; i > 0; i--) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
