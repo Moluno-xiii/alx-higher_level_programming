00f0caac8c0a6ca5ddb072705ba6ce6a1acc6890
moluno_xiii <molunoprogress@gmail.com>
2024-04-11 13:33:27 -0700

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+""" create function to add numbers """
+def add_integer(a, b=98):
+    """ adds two numbers togther """
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    elif type(b) != int:
+        raise TypeError("b must be an integer")
+    else:
+        result = int(a) + int(b)
+        return result
