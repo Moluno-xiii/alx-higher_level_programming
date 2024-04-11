8c2b07cfe0fb86d0af2a1b134eacfe7eb453c388
moluno_xiii <molunoprogress@gmail.com>
2024-04-11 09:09:26 -0700

    task 6

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Defines a class called Square """
+
+
+class Square:
+    """ Square class """
+    def __init__(self, size=0):
+        """ init function """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Getter for private attribute size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ setter for private attribute size """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ calculate the square """
+        area = self.__size ** 2
+        return area
+
+    def my_print(self):
+        """ print square with # """
+        if self.size == 0:
+            print('')
+        else:
+            for _ in range(self.size):
+                print('#' * self.size)
